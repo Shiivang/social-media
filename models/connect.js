2c95938c1cd3e3960d2301048e80332e3062b70e
Shivang4020 <107241759+Shivang4020@users.noreply.github.com>
2024-06-04 22:48:01 +0530

    db connect

--- models/connect.js
+++ models/connect.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-// const dburl = "mongodb+srv://shivang12:Shiv4020!@#@shivcluster.u61n5cz.mongodb.net/?retryWrites=true&w=majority&appName=ShivCluster"
-
 mongoose.connect("mongodb+srv://shivang12:Shiv4020@shivcluster.u61n5cz.mongodb.net/social?retryWrites=true&w=majority&appName=ShivCluster").then(()=>console.log("Connected!")).catch(()=>{
     console.log("not connected")
 });
